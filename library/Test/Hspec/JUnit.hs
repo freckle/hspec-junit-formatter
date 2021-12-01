38eb127982f55a8287f95d7e7532b8b7c0938c4d
patrick brisbin <pbrisbin@gmail.com>
2021-12-01 11:27:12 -0500

    Add hspecJUnit runners and env-based configuration
    The primary purpose of this commit is to simplify usage to just:
    
        main :: IO
        main = hspecJUnit Spec.spec
    
    This necessitated configuration through JUNIT_-prefixed environment
    variables, implemented in the new Config.Env module.
    
    In support of this, we also get:
    
    - hspecJUnitWith
    
      hspecJUnit is to hspec as hspecJUnitWith is to hspecWith. This will be
      useful in our projects that perform further per-suite configuration.
    
    - configAvailableFormatters support, when possible
    
      In hspec-2.9, we can add ourselves to the -f option for conditionally
      running with our format function. This obviates JUNIT_ENABLED, but
      we'll support that too.

--- library/Test/Hspec/JUnit.hs
+++ library/Test/Hspec/JUnit.hs
@@ -1,5 +1,14 @@
 module Test.Hspec.JUnit
-  ( configWithJUnit
+  (
+  -- * Runners
+    hspecJUnit
+  , hspecJUnitWith
+
+  -- * Directly modifying 'Config'
+  , configWithJUnit
+  , configWithJUnitAvailable
+
+  -- * Actual format function
   , junitFormat
 
   -- * Configuration
@@ -22,15 +31,54 @@ import System.Directory (createDirectoryIfMissing)
 import System.FilePath (splitFileName)
 import Test.Hspec.Core.Format
 import Test.Hspec.Core.Runner
+import Test.Hspec.Core.Runner.Ext
+import Test.Hspec.Core.Spec (Spec)
 import Test.Hspec.JUnit.Config
+import Test.Hspec.JUnit.Config.Env
 import Test.Hspec.JUnit.Render (renderJUnit)
 import qualified Test.Hspec.JUnit.Schema as Schema
 import Text.XML.Stream.Render (def, renderBytes)
+
+-- | Like 'hspec' but adds JUNit functionality
+--
+-- To actually /use/ the JUnit format, you must set @JUNIT_ENABLED=1@ in the
+-- environment; by default, this function just behaves like 'hspec'.
+--
+-- (If using hspec >= 2.9, running tests with @--test-arguments="-f junit"@ also
+-- works.)
+--
+-- All configuration of the JUnit report occurs through environment variables.
+--
+-- See "Test.Hspec.JUnit.Config" and "Test.Hspec.JUnit.Config.Env".
+--
+hspecJUnit :: Spec -> IO ()
+hspecJUnit = hspecJUnitWith defaultConfig
+
+-- | 'hspecJUnit' but built on a non-default 'Config'
+hspecJUnitWith :: Config -> Spec -> IO ()
+hspecJUnitWith config spec = do
+  junitEnabled <- envJUnitEnabled
+  junitConfig <- envJUnitConfig
+
+  let
+    modify = if junitEnabled then configWithJUnit junitConfig else id
+    base = configWithJUnitAvailable junitConfig config
+
+  hspecWith (modify base) spec
 
 -- | Modify an Hspec 'Config' to use 'junitFormat'
 configWithJUnit :: JUnitConfig -> Config -> Config
 configWithJUnit junitConfig config =
   config { configFormat = Just $ junitFormat junitConfig }
+
+-- | Modify an Hspec 'Config' to have the 'junitFormat' /available/
+--
+-- Adds @junit@ to the list of available options for @-f, --format@.
+--
+-- __NOTE__: This only works with hspec >= 2.9, otherwise it is a no-op.
+--
+configWithJUnitAvailable :: JUnitConfig -> Config -> Config
+configWithJUnitAvailable = configAddAvailableFormatter "junit" . junitFormat
 
 -- | Hspec 'configFormat' that generates a JUnit report
 junitFormat :: JUnitConfig -> FormatConfig -> IO Format
---
+++ library/Test/Hspec/JUnit/Config/Env.hs
@@ -0,0 +1,48 @@
+-- | Load a 'JUnitConfig' using environment variables
+module Test.Hspec.JUnit.Config.Env
+    ( envJUnitEnabled
+    , envJUnitConfig
+    ) where
+
+import Prelude
+
+import Data.Semigroup (Endo(..))
+import Data.Text (pack)
+import System.Directory (getCurrentDirectory)
+import System.Environment (lookupEnv)
+import System.FilePath (takeBaseName)
+import Test.Hspec.JUnit.Config
+
+-- | Is @JUNIT_ENABLED=1@ set in the environment?
+envJUnitEnabled :: IO Bool
+envJUnitEnabled = (== Just "1") <$> lookupEnv (envPrefix <> "ENABLED")
+
+-- | Produce a 'JUnitConfig' by reading environment variables
+--
+-- Variable names align with setter functions from "Test.Hspec.JUnit.Config":
+--
+-- * @JUNIT_OUTPUT_DIRECTORY@ 'setJUnitConfigOutputDirectory'
+-- * @JUNIT_OUTPUT_NAME@ 'setJUnitConfigOutputName
+-- * and so on
+--
+envJUnitConfig :: IO JUnitConfig
+envJUnitConfig = do
+  modify <- appEndo . foldMap Endo <$> sequence
+    [ lookupEnvOverride "OUTPUT_DIRECTORY" setJUnitConfigOutputDirectory
+    , lookupEnvOverride "OUTPUT_NAME" setJUnitConfigOutputName
+    , lookupEnvOverride "OUTPUT_FILE" setJUnitConfigOutputFile
+    , lookupEnvOverride "SUITE_NAME" $ setJUnitConfigSuiteName . pack
+    , lookupEnvOverride "SOURCE_PATH_PREFIX" setJUnitConfigSourcePathPrefix
+    ]
+
+  modify . defaultJUnitConfig . pack . takeBaseName <$> getCurrentDirectory
+
+lookupEnvOverride
+  :: String
+  -> (String -> JUnitConfig -> JUnitConfig)
+  -> IO (JUnitConfig -> JUnitConfig)
+lookupEnvOverride name setter =
+  maybe id setter <$> lookupEnv (envPrefix <> name)
+
+envPrefix :: String
+envPrefix = "JUNIT_"
