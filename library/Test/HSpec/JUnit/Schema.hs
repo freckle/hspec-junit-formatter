8adf20c4043115e0934cbd76b68daec1002873a2
patrick brisbin <pbrisbin@gmail.com>
2021-12-07 12:03:13 -0500

    Remove deprecated mis-cased modules
    These were kept up through v1.0.3.0 as a way to introduce
    correctly-cased modules in a backwards-compatible way. Unfortunately,
    they collide on a case-insensitive filesystem (OSX), making those
    versions inoperable. Therefore, we're going ahead with removal and a
    major version bump.

--- library/Test/HSpec/JUnit/Schema.hs
+++
@@ -1,9 +0,0 @@
-module Test.HSpec.JUnit.Schema
-  {-# DEPRECATED "Use Test.Hspec.JUnit.Schema instead" #-}
-  ( Suites(..)
-  , Suite(..)
-  , TestCase(..)
-  , Result(..)
-  ) where
-
-import Test.Hspec.JUnit.Schema
