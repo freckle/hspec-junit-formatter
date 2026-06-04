cd6c7424650c919872e0893dd2151a8b5f2e35d6
patrick brisbin <pbrisbin@gmail.com>
2026-06-04 09:49:20 -0400

    fix!: align with Testmo schema
    The goal of this was to align with Testmo (#51) and, along the way, fix
    some formatting issues (#46). To do so, I did a bit of a rewrite.
    
    The internals now make a bit more sense by being more singly-
    responsible. They came out simpler and easier to work with as a result.
    
    `Format`:
    
    This module was nominally in charge of taking Hspec values and
    translating them into our Schema types. In reality, it had grown a
    number of the rendering responsibilities, e.g. handling prefixing and
    other formatting.
    
    It has been updated to handle conversion and no more. Totals handling
    was improved as well. With a nice `SuiteTotals` monoid, we can `foldMap`
    over cases and/or nested suites and easily build what's necessary for
    parent node attributes of the same name.
    
    `Schema`:
    
    This module was nominally to represent the structure of our desired XML.
    It too shared in some rendering concerns, and it didn't *exactly* match
    the XML structure we intend to produce -- which is why `Format` had to
    do more work and `Render` had to be more complex.
    
    It now exactly matches our desired output, which was made to match the
    Testmo specification. For example, if there is a `testuite` node, you
    will find a `Testsuite` type (not `Suite`). If a `testcase` node has a
    `file` and `line` attribute, you will find `file` and `line` attributes
    on the `Testcase` type (not `location`). The `testsuites` node is
    represented structurally at this level, and the parent-child
    relationships are modeled in type-safe ways.
    
    `Render`:
    
    Now that `Schema` is an accurate and complete representation of our
    desired XML, this module only needs to serialize that structure in a
    straight-ahead manner. This frees its complexity budget to tackle actual
    rendering concerns like file-prefixing and text sanitization -- e.g.
    fixing #46.
    
    This module was the one that cared about `JUnitConfig`, and it cared
    about it throughout deep function calls. To make this easier, I
    structured things with `MonadReader JUnitConfig`, so you could access
    those options at any depth of call.
    
    The tests did require updating as part of this.
    
    I simplified the grouping logic to use `NonEmpty.groupAllWith` instead
    of `Map.fromListWith (<>)`. The latter was reversing the order of
    `testcase` nodes from what was in the actual spec. I think having the
    order match the source is better, so I had to update the test suite
    accordingly, which will muddy the diff quite a bit.
    
    I stopped prefixing messages with location information. We did that back
    before we had the `file` and `line` attributes. These seem preferred now
    and keeping message prefixing didn't work well with the new `failure`
    and `skipped` structure (which use `message`).
    
    We also now correctly distinguish `errors` from `failures`, so that
    required test changes.
    
    I did not tackle putting things into a tree structure, so that nested
    `context`/`describe` would come out as nested `testsuite` nodes. I think
    it'd be great to have this, but building it from the flat `[(Path,
    Item)]` data that Hspec gives us was too hard for me.

--- library/Test/Hspec/JUnit/Render.hs
+++ library/Test/Hspec/JUnit/Render.hs
@@ -1,103 +1,215 @@
 module Test.Hspec.JUnit.Render
-  ( renderJUnit
+  ( renderTestsuites
   ) where
 
 import Prelude
 
-import Conduit (ConduitT, awaitForever, mergeSource, yield, yieldMany, (.|))
-import Control.Monad.Catch (MonadThrow)
+import Conduit (ConduitT)
+import Control.Monad (unless)
+import Control.Monad.Reader (MonadReader (..), asks)
 import Data.Array qualified as Array
-import Data.Conduit.List qualified as CL
 import Data.Foldable (traverse_)
+import Data.Semigroup (Endo (..))
 import Data.Text (Text, pack)
-import Data.Text qualified as Text
+import Data.Text qualified as T
 import Data.Time.ISO8601 (formatISO8601)
 import Data.XML.Types (Event)
+import Test.Hspec.JUnit.Config
 import Test.Hspec.JUnit.Schema
-  ( Location (..)
-  , Result (..)
-  , Suite (..)
-  , Suites (..)
-  , TestCase (..)
-  )
 import Text.Printf
 import Text.Regex.Base qualified as Regex
-import Text.Regex.TDFA.Text qualified as Regex
-import Text.XML.Stream.Render (attr, content, tag)
+import Text.Regex.TDFA.Text (Regex)
+import Text.XML.Stream.Render (attr, content, optionalAttr, tag)
 
-renderJUnit :: MonadThrow m => Bool -> ConduitT Suites Event m ()
-renderJUnit shouldDropConsoleFormatting = awaitForever $ \s ->
-  tag "testsuites" (attr "name" s.name)
-    $ yieldMany s.suites
-      .| mergeSource idStream
-      .| suite shouldDropConsoleFormatting
+renderTestsuites
+  :: MonadReader JUnitConfig m
+  => Testsuites
+  -> ConduitT i Event m ()
+renderTestsuites s = do
+  tag
+    "testsuites"
+    ( attr "name" (unAttr id s.name)
+        <> attr "tests" (unAttr (pack . show) s.tests)
+        <> attr "failures" (unAttr (pack . show) s.failures)
+        <> attr "errors" (unAttr (pack . show) s.errors)
+        <> attr "skipped" (unAttr (pack . show) s.skipped)
+        <> attr "assertions" (unAttr (pack . show) s.assertions)
+        <> attr "time" (unAttr unSeconds s.time)
+        <> attr "timestamp" (unAttr (pack . formatISO8601) s.timestamp)
+    )
+    $ traverse_ renderTestsuite s.children
+
+renderTestsuite
+  :: MonadReader JUnitConfig m
+  => Testsuite
+  -> ConduitT i Event m ()
+renderTestsuite s = do
+  prefix <- asks getJUnitPrefixSourcePath
+
+  tag
+    "testsuite"
+    ( attr "name" (unAttr id s.name)
+        <> attr "tests" (unAttr (pack . show) s.tests)
+        <> attr "failures" (unAttr (pack . show) s.failures)
+        <> attr "errors" (unAttr (pack . show) s.errors)
+        <> attr "skipped" (unAttr (pack . show) s.skipped)
+        <> attr "assertions" (unAttr (pack . show) s.assertions)
+        <> attr "time" (unAttr unSeconds s.time)
+        <> attr "timestamp" (unAttr (pack . formatISO8601) s.timestamp)
+        <> optionalAttr "file" (unAttr (pack . prefix) <$> s.file)
+    )
+    $ traverse_ renderTestsuiteChild s.children
+
+renderTestsuiteChild
+  :: MonadReader JUnitConfig m
+  => TestsuiteChild
+  -> ConduitT i Event m ()
+renderTestsuiteChild = \case
+  TestsuiteProperties x -> renderProperties x
+  TestsuiteSystemOut x -> renderSystemOut x
+  TestsuiteSystemErr x -> renderSystemErr x
+  TestsuiteTestcase x -> renderTestcase x
+  TestsuiteTestsuite x -> renderTestsuite x
+
+renderProperties
+  :: MonadReader JUnitConfig m
+  => Properties
+  -> ConduitT i Event m ()
+renderProperties ps = unless (null ps.unwrap) $ do
+  tag "properties" mempty $ traverse_ renderProperty $ ps.unwrap
+
+renderProperty
+  :: MonadReader JUnitConfig m
+  => Property
+  -> ConduitT i Event m ()
+renderProperty p = case p.value of
+  Left a ->
+    tag "property" (attr "name" (unAttr id p.name) <> attr "value" (unAttr id a))
+      $ pure ()
+  Right c -> tag "property" (attr "name" (unAttr id p.name)) $ renderContent c
+
+renderSystemOut
+  :: MonadReader JUnitConfig m
+  => SystemOut
+  -> ConduitT i Event m ()
+renderSystemOut = tag "system-out" mempty . renderContent . (.unwrap)
+
+renderSystemErr
+  :: MonadReader JUnitConfig m
+  => SystemErr
+  -> ConduitT i Event m ()
+renderSystemErr = tag "system-err" mempty . renderContent . (.unwrap)
+
+renderTestcase
+  :: MonadReader JUnitConfig m
+  => Testcase
+  -> ConduitT i Event m ()
+renderTestcase c = do
+  prefix <- asks getJUnitPrefixSourcePath
+
+  tag
+    "testcase"
+    ( attr "name" (unAttr id c.name)
+        <> attr "classname" (unAttr id c.classname)
+        <> attr "assertions" (unAttr (pack . show) c.assertions)
+        <> attr "time" (unAttr unSeconds c.time)
+        <> optionalAttr "file" (unAttr (pack . prefix) <$> c.file)
+        <> optionalAttr "line" (unAttr (pack . show) <$> c.line)
+    )
+    $ do
+      traverse_ renderProperties c.properties
+      traverse_ (either renderSystemOut renderSystemErr) c.systemStream
+      renderTestcaseChild c.child
+
+renderTestcaseChild
+  :: MonadReader JUnitConfig m
+  => TestcaseChild
+  -> ConduitT i Event m ()
+renderTestcaseChild = \case
+  TestcaseEmpty -> pure ()
+  TestcaseSkipped x -> renderSkipped x
+  TestcaseFailure x -> renderFailure x
+  TestcaseError x -> renderError x
+
+renderSkipped
+  :: MonadReader JUnitConfig m
+  => Skipped
+  -> ConduitT i Event m ()
+renderSkipped s = tag "skipped" (attr "message" (unAttr id s.message)) $ pure ()
+
+renderFailure
+  :: MonadReader JUnitConfig m
+  => Failure
+  -> ConduitT i Event m ()
+renderFailure f =
+  tag
+    "failure"
+    (attr "message" (unAttr id f.message) <> attr "type" (unAttr id f.type_))
+    $ renderContent f.content
+
+renderError
+  :: MonadReader JUnitConfig m
+  => Error
+  -> ConduitT i Event m ()
+renderError e =
+  tag
+    "error"
+    (attr "message" (unAttr id e.message) <> attr "type" (unAttr id e.type_))
+    $ renderContent e.content
+
+renderContent
+  :: MonadReader JUnitConfig m
+  => Content
+  -> ConduitT i Event m ()
+renderContent c = do
+  shouldDropConsoleFormatting <- asks getJUnitConfigDropConsoleFormatting
+
+  let clean =
+        if shouldDropConsoleFormatting
+          then dropConsoleFormatting
+          else id
+
+  content $ clean $ c.unwrap
+
+unSeconds :: Seconds -> Text
+unSeconds (Seconds d) = pack $ printf "%0.9f" d
+
+-- | Safely render an attribute
+--
+-- HTML encoding is handled by @xml-conduit@, but additionally we:
+--
+-- * Drop console formatting (always)
+-- * Extra-escape unsupported characters (e.g @\a@ becomes @\\a@)
+unAttr :: (a -> Text) -> Attr a -> Text
+unAttr f = dropConsoleFormatting . escapeIllegal . f . (.unwrap)
+
+escapeIllegal :: Text -> Text
+escapeIllegal =
+  appEndo
+    $ foldMap
+      (\(a, b) -> Endo $ T.replace a b)
+      [ ("\0", "\\0")
+      , ("\a", "\\a")
+      , ("\b", "\\b")
+      , ("\f", "\\f")
+      , ("\v", "\\v")
+      , ("\ESC", "\\ESC")
+      ]
+
+-- | Drop ANSI control characters which might be used to set colors
+--
+-- We always do this in attribute values, since they'd only break XML, but we
+-- only do it in content if configured by options, since we want any content
+-- node changes to be opt-in.
+dropConsoleFormatting :: Text -> Text
+dropConsoleFormatting input =
+  foldr (dropBetween . (Array.! 0)) input $ Regex.matchAll escRegex input
+
+escRegex :: Regex
+escRegex = Regex.makeRegex ("\x1b\\[[0-9;]*[mGKHF]" :: Text)
+
+dropBetween :: (Int, Int) -> Text -> Text
+dropBetween (offset, len) input = begining <> end
  where
-  idStream = CL.iterate (+ 1) 0
-
-suite :: MonadThrow m => Bool -> ConduitT (Int, Suite) Event m ()
-suite shouldDropConsoleFormatting = awaitForever $ \(i, s) ->
-  tag "testsuite" (attributes i s) $ do
-    tag "properties" mempty mempty
-    yieldMany s.cases .| testCase shouldDropConsoleFormatting
- where
-  -- TODO these need to be made real values
-  attributes i s =
-    attr "name" s.name
-      <> attr "package" s.name
-      <> attr "id" (tshow i)
-      <> attr "time" (roundToStr $ sumDurations s.cases)
-      <> attr "timestamp" (pack $ formatISO8601 s.timestamp)
-      <> attr "hostname" "localhost"
-      <> attr "tests" (tshow $ length s.cases)
-      <> attr "failures" (tshow $ length [() | Just Failure {} <- (.result) <$> s.cases])
-      <> attr "errors" "0"
-      <> attr "skipped" (tshow $ length [() | Just Skipped {} <- (.result) <$> s.cases])
-
-tshow :: Show a => a -> Text
-tshow = pack . show
-
-testCase :: MonadThrow m => Bool -> ConduitT TestCase Event m ()
-testCase shouldDropConsoleFormatting =
-  awaitForever $ \(TestCase mLocation className name duration mResult) ->
-    tag "testcase" (attributes mLocation className name duration)
-      $ traverse_ yield mResult
-        .| result shouldDropConsoleFormatting
- where
-  attributes mLocation className name duration =
-    maybe mempty (attr "file" . pack . (.file)) mLocation
-      <> maybe mempty (attr "line" . pack . show . (.line)) mLocation
-      <> attr "name" name
-      <> attr "classname" className
-      <> attr "time" (roundToStr duration)
-
-result :: MonadThrow m => Bool -> ConduitT Result Event m ()
-result shouldDropConsoleFormatting = awaitForever go
- where
-  go (Failure fType contents) =
-    tag "failure" (attr "type" fType) $ content $ dropConsoleFormatting contents
-  go (Skipped contents) = tag "skipped" mempty $ content $ dropConsoleFormatting contents
-
-  -- Drops ANSI control characters which might be used to set colors.
-  -- Including these breaks XML, there is not much point encoding them.
-  dropConsoleFormatting :: Text -> Text
-  dropConsoleFormatting input
-    | shouldDropConsoleFormatting =
-        let
-          regex = Regex.makeRegex (pack "\x1b\\[[0-9;]*[mGKHF]") :: Regex.Regex
-          matches = Regex.matchAll regex input
-          dropMatch (offset, len) input' =
-            let
-              (begining, rest) = Text.splitAt offset input'
-              (_, end) = Text.splitAt len rest
-            in
-              begining <> end
-          matchTuples = map (Array.! 0) matches
-        in
-          foldr dropMatch input matchTuples
-    | otherwise = input
-
-sumDurations :: [TestCase] -> Double
-sumDurations cases = sum $ (.duration) <$> cases
-
-roundToStr :: PrintfArg a => a -> Text
-roundToStr = pack . printf "%0.9f"
+  (begining, rest) = T.splitAt offset input
+  (_, end) = T.splitAt len rest
--- tests/Test/Hspec/JUnit/FormatterSpec.hs
+++ tests/Test/Hspec/JUnit/FormatterSpec.hs
@@ -39,29 +39,11 @@ spec = do
           )
           `shouldSatisfy` (== 2) . length
 
-      it "has hostname" $ do
-        ( doc
-            $| element "testsuites"
-            &/ element "testsuite"
-            &| attribute "hostname"
-          )
-          `shouldBe` replicate 2 ["localhost"]
-
       it "has name" $ do
         ( doc
             $| element "testsuites"
             &/ element "testsuite"
             &| attribute "name"
-          )
-          `shouldBe` [ ["Some section"]
-                     , ["Some section/A grouped context"]
-                     ]
-
-      it "has package" $ do
-        ( doc
-            $| element "testsuites"
-            &/ element "testsuite"
-            &| attribute "package"
           )
           `shouldBe` [ ["Some section"]
                      , ["Some section/A grouped context"]
@@ -114,6 +96,21 @@ spec = do
           )
           `shouldBe` replicate 6 ["tests/Example.hs"]
 
+      it "has names" $ do
+        ( doc
+            $| element "testsuites"
+            &/ element "testsuite"
+            &/ element "testcase"
+            &| attribute "name"
+          )
+          `shouldBe` [ ["has an expectation"]
+                     , ["has a failure"]
+                     , ["happens in a group"]
+                     , ["also happens in a group"]
+                     , ["gets skipped"]
+                     , ["throws a colourful exception"]
+                     ]
+
     context "first testsuite" $ do
       let [testsuite, _] = doc $| element "testsuites" &/ element "testsuite"
 
@@ -130,6 +127,18 @@ spec = do
         let [testcase, _] = testsuite $/ element "testcase"
 
         it "has line" $ do
+          (testcase $| attribute "line") `shouldBe` ["16"]
+
+        it "has name" $ do
+          (testcase $| attribute "name") `shouldBe` ["has an expectation"]
+
+        it "has no child nodes" $ do
+          (testcase $| child &| node) `shouldBe` []
+
+      context "second testcase" $ do
+        let [_, testcase] = testsuite $/ element "testcase"
+
+        it "has line" $ do
           (testcase $| attribute "line") `shouldBe` ["19"]
 
         it "has name" $ do
@@ -141,24 +150,10 @@ spec = do
               &/ content
             )
             `shouldBe` [ T.unlines
-                           [ "tests/Example.hs:19:12"
-                           , ""
-                           , "expected: \"False\""
+                           [ "Expected: \"False\""
                            , " but got: \"True\""
                            ]
                        ]
-
-      context "second testcase" $ do
-        let [_, testcase] = testsuite $/ element "testcase"
-
-        it "has line" $ do
-          (testcase $| attribute "line") `shouldBe` ["16"]
-
-        it "has name" $ do
-          (testcase $| attribute "name") `shouldBe` ["has an expectation"]
-
-        it "has no child nodes" $ do
-          (testcase $| child &| node) `shouldBe` []
 
     context "second testsuite" $ do
       let [_, testsuite] =
@@ -167,8 +162,8 @@ spec = do
               &/ element "testsuite"
 
       it "has correct counts" $ do
-        (testsuite $| attribute "errors") `shouldBe` ["0"]
-        (testsuite $| attribute "failures") `shouldBe` ["1"]
+        (testsuite $| attribute "errors") `shouldBe` ["1"]
+        (testsuite $| attribute "failures") `shouldBe` ["0"]
         (testsuite $| attribute "skipped") `shouldBe` ["1"]
         (testsuite $| attribute "tests") `shouldBe` ["4"]
 
@@ -177,6 +172,46 @@ spec = do
 
       context "first testcase" $ do
         let [testcase, _, _, _] = testsuite $/ element "testcase"
+
+        it "has line" $ do
+          (testcase $| attribute "line") `shouldBe` ["22"]
+
+        it "has name" $ do
+          (testcase $| attribute "name") `shouldBe` ["happens in a group"]
+
+        it "has no child nodes" $ do
+          (testcase $| child &| node) `shouldBe` []
+
+      context "second testcase" $ do
+        let [_, testcase, _, _] = testsuite $/ element "testcase"
+
+        it "has line" $ do
+          (testcase $| attribute "line") `shouldBe` ["24"]
+
+        it "has name" $ do
+          (testcase $| attribute "name") `shouldBe` ["also happens in a group"]
+
+        it "has no child nodes" $ do
+          (testcase $| child &| node) `shouldBe` []
+
+      context "third testcase" $ do
+        let [_, _, testcase, _] = testsuite $/ element "testcase"
+
+        it "has line" $ do
+          (testcase $| attribute "line") `shouldBe` ["27"]
+
+        it "has name" $ do
+          (testcase $| attribute "name") `shouldBe` ["gets skipped"]
+
+        it "has a skipped node" $ do
+          ( testcase
+              $/ element "skipped"
+              &| attribute "message"
+            )
+            `shouldBe` [["some reason"]]
+
+      context "fourth testcase" $ do
+        let [_, _, _, testcase] = testsuite $/ element "testcase"
 
         it "has line" $ do
           pendingWith "Newer base returns line 29"
@@ -193,50 +228,6 @@ spec = do
             )
             `shouldBe` ["ColourfulException"]
 
-      context "second testcase" $ do
-        let [_, testcase, _, _] = testsuite $/ element "testcase"
-
-        it "has line" $ do
-          (testcase $| attribute "line") `shouldBe` ["27"]
-
-        it "has name" $ do
-          (testcase $| attribute "name") `shouldBe` ["gets skipped"]
-
-        it "has a skipped node" $ do
-          ( testcase
-              $/ element "skipped"
-              &/ content
-            )
-            `shouldBe` [ mconcat -- no trailing newline on this one (bug?)
-                           [ "tests/Example.hs:27:9\n"
-                           , "some reason"
-                           ]
-                       ]
-
-      context "third testcase" $ do
-        let [_, _, testcase, _] = testsuite $/ element "testcase"
-
-        it "has line" $ do
-          (testcase $| attribute "line") `shouldBe` ["24"]
-
-        it "has name" $ do
-          (testcase $| attribute "name") `shouldBe` ["also happens in a group"]
-
-        it "has no child nodes" $ do
-          (testcase $| child &| node) `shouldBe` []
-
-      context "fourth testcase" $ do
-        let [_, _, _, testcase] = testsuite $/ element "testcase"
-
-        it "has line" $ do
-          (testcase $| attribute "line") `shouldBe` ["22"]
-
-        it "has name" $ do
-          (testcase $| attribute "name") `shouldBe` ["happens in a group"]
-
-        it "has no child nodes" $ do
-          (testcase $| child &| node) `shouldBe` []
-
   context "ExampleSpec with prefixing" $ do
     let testConfig' = setJUnitConfigSourcePathPrefix "lol/monorepo" testConfig
     doc <- runIO $ fromDocument <$> renderJUnitXml testConfig' Example.spec
@@ -249,27 +240,6 @@ spec = do
           &| attribute "file"
         )
         `shouldBe` replicate 6 ["lol/monorepo/tests/Example.hs"]
-
-    it "prefixes failure messages" $ do
-      pendingWith "The ColourfulException failure does not get prefixed"
-      ( doc
-          $| element "testsuites"
-          &/ element "testsuite"
-          &/ element "testcase"
-          &/ element "failure"
-          &| content
-        )
-        `shouldSatisfy` all (all ("lol/monorepo/" `T.isPrefixOf`))
-
-    it "prefixes skipped messages" $ do
-      ( doc
-          $| element "testsuites"
-          &/ element "testsuite"
-          &/ element "testcase"
-          &/ element "skipped"
-          &| content
-        )
-        `shouldSatisfy` all (all ("lol/monorepo/" `T.isPrefixOf`))
 
 testConfig :: JUnitConfig
 testConfig = defaultJUnitConfig "hspec-junit-format"
